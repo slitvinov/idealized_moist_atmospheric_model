e060f4ac15cbed412d0609405280899bfe4663ab
Sergey Litvinov <slitvinov@gmail.com>
2025-07-20 12:41:16 +0200

    compile.sh

--- exp/compile.sh
+++ exp/compile.sh
@@ -9,7 +9,8 @@ pathnames=../input/path_names
 ppdir=../postprocessing
 execdir=exec.$platform
 executable=$execdir/idealized_moist.x
-mkdir -p /home/lisergey/idealized_moist_atmospheric_model/exp/exec.gcc
-cd /home/lisergey/idealized_moist_atmospheric_model/exp/exec.gcc
-/home/lisergey/idealized_moist_atmospheric_model/exp/../bin/mkmf -a /home/lisergey/idealized_moist_atmospheric_model/exp/../src -t /home/lisergey/idealized_moist_atmospheric_model/exp/../bin/mkmf.template.gcc -p idealized_moist.x -c "-Duse_libMPI -Duse_netCDF -Duse_LARGEFILE -DINTERNAL_FILE_NML -DOVERLOAD_C8" /home/lisergey/idealized_moist_atmospheric_model/exp/../input/path_names /home/lisergey/idealized_moist_atmospheric_model/exp/../src/shared/include /home/lisergey/idealized_moist_atmospheric_model/exp/../src/shared/mpp/include
+mkdir -p exec.gcc
+p=`pwd`
+cd exec.gcc
+../../bin/mkmf -a $p/../src -t $p/../bin/mkmf.template.gcc -p idealized_moist.x -c "-Duse_libMPI -Duse_netCDF -Duse_LARGEFILE -DINTERNAL_FILE_NML -DOVERLOAD_C8" $p/../input/path_names $p/../src/shared/include $p/../src/shared/mpp/include
 make all
