40f0b54d39267f9c08bd7b0da3c29b29f1b5bafb
Sergey Litvinov <slitvinov@gmail.com>
2025-07-20 14:40:44 +0200

    up

--- src/shared/include/fms_platform.h
+++ src/shared/include/fms_platform.h
@@ -3,16 +3,14 @@
 
 #define DOUBLE_KIND 8
 #define FLOAT_KIND 4
-#define LONG_KIND 8
-#define INT_KIND 4
+#define LONG_KIND 16
+#define INT_KIND 8
 #define SHORT_KIND 2
 #define POINTER_KIND 4
-
 #define NO_QUAD_PRECISION
-#define _PURE PURE
-
-#define _ALLOCATABLE POINTER
-#define _NULL =>NULL()
-#define _ALLOCATED ASSOCIATED
+#define _ALLOCATABLE ALLOCATABLE
+#define _ALLOCATED ALLOCATED
+#define _NULL
+#define _PURE
 
 #endif
