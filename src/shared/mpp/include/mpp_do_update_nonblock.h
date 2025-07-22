4b9c4318d3cc5e7e578c8a07239a5347a1e00596
Sergey Litvinov <slitvinov@gmail.com>
2025-07-22 16:31:44 +0200

    use mpi

--- src/shared/mpp/include/mpp_do_update_nonblock.h
+++ src/shared/mpp/include/mpp_do_update_nonblock.h
@@ -1,6 +1,9 @@
 
 ! -*-f90-*- 
 subroutine MPP_START_DO_UPDATE_3D_(id_update, f_addrs, domain, update, d_type, ke_max, ke_list, flags, reuse_id_update, name)
+#ifdef use_libMPI
+  use mpi
+#endif
   integer,                    intent(in) :: id_update
   integer(LONG_KIND),         intent(in) :: f_addrs(:,:)
   type(domain2D),             intent(in) :: domain
@@ -238,7 +241,10 @@ end subroutine MPP_START_DO_UPDATE_3D_
 !###############################################################################
 
 subroutine MPP_COMPLETE_DO_UPDATE_3D_(id_update, f_addrs, domain, update, d_type, ke_max, ke_list, &
-                                      b_addrs, b_size, flags) 
+     b_addrs, b_size, flags)
+#ifdef use_libMPI
+  use mpi
+#endif
   integer,             intent(in) :: id_update
   integer(LONG_KIND),  intent(in) :: f_addrs(:,:)
   type(domain2d),      intent(in) :: domain
