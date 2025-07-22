d0325b6e87c134cbbc352a73dbee6c519048056b
Sergey Litvinov <slitvinov@gmail.com>
2025-07-22 14:08:00 +0200

    up

--- exp/compile.sh
+++ exp/compile.sh
@@ -2,10 +2,8 @@
 set -eu
 
 platform=gcc
-mkdir -p exec.gcc
 p=`pwd`
-cd exec.gcc
-../../bin/mkmf -a $p/../src -t $p/../bin/mkmf.template.gcc -p idealized_moist.x \
+../bin/mkmf -a $p/../src -t $p/../bin/mkmf.template.gcc -p idealized_moist.x \
     -c "-Duse_libMPI -Duse_netCDF -Duse_LARGEFILE -DINTERNAL_FILE_NML -DOVERLOAD_C8" \
     $p/../input/path_names $p/../src/shared/include $p/../src/shared/mpp/include
 make all
