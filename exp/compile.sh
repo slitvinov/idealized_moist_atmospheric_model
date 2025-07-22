e8d0b5990fcb8407759f2f18a8c115528a62bd84
Sergey Litvinov <slitvinov@gmail.com>
2025-07-22 14:12:55 +0200

    use ./configure

--- exp/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-platform=gcc
-p=`pwd`
-../bin/mkmf -a $p/../src -t $p/../bin/mkmf.template.gcc -p idealized_moist.x \
-    -c "-Duse_libMPI -Duse_netCDF -Duse_LARGEFILE -DINTERNAL_FILE_NML -DOVERLOAD_C8" \
-    $p/../input/path_names $p/../src/shared/include $p/../src/shared/mpp/include
-make all
